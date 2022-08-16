a54b2e2359bc7b65acac2588eb7f64fd59146c9e
unknown <droid64down@gmail.com>
2022-08-16 18:02:26 -0400

    Implemented password generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,54 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var passLength, charTypes;
-
-  passLength = prompt("Enter the desired password length (8-128)", 8);
-  charTypes = prompt("Choose at least one or more character types to include. \n 1 - Lowercase, 2 - Uppercase, 3 - Numeric, 4 - Special Characters");
-
-
 
   passwordText.value = password;
+}
 
+// Password generation function
+function generatePassword() {
+  var lengthCheck = false;
+  var charTypeCheck = false;
+
+  while(!(lengthCheck && charTypeCheck)){
+    var passLength = prompt("Enter the desired password length (8-128)", 8);
+    var upperInclude = prompt("Include Uppercase characters? Y/N").toUpperCase();
+    var lowerInclude = prompt("Include Lowercase characters? Y/N").toUpperCase();
+    var numInclude = prompt("Include Numbers? Y/N").toUpperCase();
+    var specInclude = prompt("Include Special characters? Y/N").toUpperCase();
+
+    if(passLength != "") {lengthCheck = true}
+    if(upperInclude != "") {charTypeCheck = true}
+    if(lowerInclude != "") {charTypeCheck = true}
+    if(numInclude != "") {charTypeCheck = true}
+    if(specInclude != "") {charTypeCheck = true}
+  }
+  
+  var passHolder = "";
+  var chars = "";
+
+  if(upperInclude == "Y"){
+    chars+="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if(lowerInclude == "Y"){
+    chars+="abcdefghijklmnopqrstuvwxyz"
+  }
+  if(numInclude == "Y"){
+    chars+="0123456789"
+  }
+  if(specInclude == "Y"){
+    chars+="!@#$%^&*()"
+  }
+
+  for(var i = 0; i<passLength; i++){
+    var charVal = Math.round(Math.random() * chars.length);
+    passHolder+=chars[charVal];
+  }
+  return passHolder;
 }
 
 // Add event listener to generate button
